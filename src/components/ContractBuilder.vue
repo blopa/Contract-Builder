8043738033158cc5dac61cfc62be0c2be99c571e
blopa <montenegro.pablo@outlook.com>
2017-10-08 00:32:25 -0300

    building new algo

--- src/components/ContractBuilder.vue
+++ src/components/ContractBuilder.vue
@@ -25,6 +25,7 @@
       return {
         decisions: this.$store.state.decisionsTree,
         current: this.$store.state.currentNode,
+        auxPath: [],
         button: false
       }
     },
@@ -34,8 +35,57 @@
         this.JSONPath(this.decisions, 0) // first call to genHTML and choices
       },
       JSONPath (json, nodeIndex) {
+        var $this = this
         debugger
-        console.log('worked')
+        console.log('Current node: ' + nodeIndex)
+        var pickOption = ''
+        var found = false
+        var tempPaths = []
+
+        json.forEach(function (item) {
+          debugger
+          if (!found) {
+            if (item.mandatory) {
+              // generate HTML
+              $this.generateHTMLContent(item)
+              if (item.childs.length > 0) {
+                found = $this.JSONPath(item.childs, nodeIndex + 1)
+              }
+            } else {
+              // generate choices
+              $this.current = item
+              $this.generateChoice(item)
+              found = true
+            }
+          } else {
+            tempPaths.push(item)
+          }
+        })
+        if (tempPaths.length > 0) {
+          this.auxPath.push(tempPaths)
+        }
+        if ((!found) && (this.decisions.length > 0)) {
+          debugger
+          found = this.JSONPath(this.decisions, nodeIndex + 1)
+        }
+        if (nodeIndex === 0) {
+          debugger
+          var len = this.auxPath.length
+          for (var i = (len - 1); i >= 0; i--) {
+            this.decisions = this.auxPath[i].concat(this.decisions) // merges arrays
+          }
+          this.auxPath = []
+          if (this.decisions.length <= 0) {
+            pickOption.hide()
+          }
+        }
+        return found
+      },
+      generateHTMLContent (item) {
+        debugger
+      },
+      generateChoice (item) {
+        debugger
       }
     }
   }
